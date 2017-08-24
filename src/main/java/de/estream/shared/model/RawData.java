0c85b34d33ff8be5744a523e96db11f4225aa2ff
FlorianDe <florian.decker@uni-oldenburg.de>
2017-08-25 00:45:45 +0200

    Init Commit

---
+++ src/main/java/de/estream/shared/model/RawData.java
@@ -0,0 +1,11 @@
+package de.estream.shared.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * Created by Florian on 25.08.2017.
+ */
+public class RawData {
+    @JsonProperty
+    public String TestData;
+}
